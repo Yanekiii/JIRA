a3f4c6772ef865641af12ac7ce72e6a954e24ab4
Yanekiii <yassinessafhi1509@gmail.com>
2026-03-15 17:13:57 +0100

    UPDATE SITE

--- ProjectJira-main/django_project/blog/admin.py
+++ ProjectJira-main/django_project/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Ticket, Project
+from .models import Project, ProjectMembership, Sprint, Epic, Ticket
 
-admin.site.register(Ticket)
 admin.site.register(Project)
+admin.site.register(ProjectMembership)
+admin.site.register(Sprint)
+admin.site.register(Epic)
+admin.site.register(Ticket)
